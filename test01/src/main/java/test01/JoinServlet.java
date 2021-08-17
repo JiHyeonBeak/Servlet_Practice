bb02828def3c91cadd7a8bc94ab4ad779b05bc8f
JiHyeonBeak <jadehl09@gmail.com>
2021-08-17 22:48:53 +0900

    First Commit

---
+++ test01/src/main/java/test01/JoinServlet.java
@@ -0,0 +1,29 @@
+package test01;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JoinServlet
+ */
+@WebServlet("/joinserv")
+public class JoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dis = request.getRequestDispatcher("Join.html");
+		dis.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
