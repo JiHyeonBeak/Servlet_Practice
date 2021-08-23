0b6765d46e8a0dce34f6f8f0bab30288ab77981a
JiHyeonBeak <jadehl09@gmail.com>
2021-08-23 22:03:39 +0900

    fix : now it can oracle connection!

--- test_dao/src/main/java/sec01/ex01/MemberDAO.java
+++ test_dao/src/main/java/sec01/ex01/MemberDAO.java
@@ -3,28 +3,36 @@ package sec01.ex01;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.sql.*;
 
 public class MemberDAO {
 	private Connection con;
-	private Statement stmt;
+	private PreparedStatement pstmt;
+	
+	public MemberDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver"); // ojdbc6.jar 를 가지고 와야 했는데 안 되어 있었다.
+		} catch(Exception e) {
+			System.out.println("드라이버 인식 안됨");
+		}
+	}
 	
 	private void connDB() {
 		try {
-			String url = "jdbc:Oracle:thin:@localhost:1521:xe";
+			String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
 			String user = "system";
 			String pwd = "oracle";
 			con = DriverManager.getConnection(url, user, pwd);
 			System.out.println("오라클 접속");
-		}catch(Exception e) {
+			}	
+			catch(Exception e) {
 			e.printStackTrace();
 			System.out.println("접속 실패");
 		}
 	}
-	
 
 	public List listMembers() {
 		List list = new ArrayList();
@@ -32,7 +40,8 @@ public class MemberDAO {
 			connDB();
 			String sql = "select * from t_member";
 			System.out.println(sql);
-			ResultSet rs = stmt.executeQuery(sql);
+			pstmt = con.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery(sql);
 			while(rs.next()) {
 				String id = rs.getString("id");
 				String pwd = rs.getString("pwd");
@@ -48,7 +57,7 @@ public class MemberDAO {
 				list.add(vo);
 			}
 			rs.close();
-			stmt.close();
+			pstmt.close();
 			con.close();
 		}catch (Exception e) {
 			e.printStackTrace();
